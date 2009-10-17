d2c3de4ecc55dcd95c7daaea4d26cfe498639189
Thomas Allmer <thomas.allmer@webteam.at>
2009-10-17 11:45:50 +0200

    sugguest mpm

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,6 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'conflicts' => array(
 		),
 		'suggests' => array(
+			'mpm' => '',
 		),
 	),
 	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"16e2";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"0107";s:19:"doc/wizard_form.dat";s:4:"fd02";s:20:"doc/wizard_form.html";s:4:"ed7e";s:13:"mod1/conf.php";s:4:"05ac";s:14:"mod1/index.php";s:4:"1fb9";s:18:"mod1/locallang.xml";s:4:"d5ca";s:22:"mod1/locallang_mod.xml";s:4:"3b58";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
