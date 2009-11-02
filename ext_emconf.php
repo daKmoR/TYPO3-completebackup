b026fbe8ede557fcee41366ba30e866d3cfe4cc0
Thomas Allmer <daKmoR@gmx.at>
2009-11-02 18:07:01 +0100

    TER 0.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "completebackup"
 #
-# Auto generated 04-06-2009 18:49
+# Auto generated 02-11-2009 17:58
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -39,7 +39,9 @@ $EM_CONF[$_EXTKEY] = array(
 			'mpm' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"16e2";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"0107";s:19:"doc/wizard_form.dat";s:4:"fd02";s:20:"doc/wizard_form.html";s:4:"ed7e";s:13:"mod1/conf.php";s:4:"05ac";s:14:"mod1/index.php";s:4:"1fb9";s:18:"mod1/locallang.xml";s:4:"d5ca";s:22:"mod1/locallang_mod.xml";s:4:"3b58";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"d66a";s:10:"README.txt";s:4:"9fa9";s:21:"ext_conf_template.txt";s:4:"8a7b";s:12:"ext_icon.gif";s:4:"809e";s:17:"ext_localconf.php";s:4:"53e4";s:14:"ext_tables.php";s:4:"0ef6";s:35:"eid/class.tx_completebackup_eid.php";s:4:"d108";s:22:"mod1/completebackup.js";s:4:"5fff";s:13:"mod1/conf.php";s:4:"3458";s:14:"mod1/index.php";s:4:"eb1e";s:22:"mod1/locallang_mod.xml";s:4:"e70b";s:19:"mod1/moduleicon.gif";s:4:"809e";s:20:"Resources/backup.php";s:4:"6b58";s:20:"Resources/deploy.php";s:4:"aace";s:37:"Resources/Php/class.AdvZipArchive.php";s:4:"1f8f";s:33:"Resources/Php/class.MySQLDump.php";s:4:"2873";s:27:"Resources/Php/class.Tar.php";s:4:"aa1b";}',
+	'suggests' => array(
+	),
 );
 
 ?>
